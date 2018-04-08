4fd33f0e0105c4af29f86f02b481c218ea1e0178
Kasia.Michalowska <katrmich@gmail.com>
2018-04-08 17:54:03 +0200

    Initial commit

---
+++ autoTests/src/test/java/com/infoshare/suiteTest/FeatureTestSuite.java
@@ -0,0 +1,10 @@
+package com.infoshare.suiteTest;
+
+import com.infoshare.tests.FutrzakTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({FutrzakTest.class})
+public class FeatureTestSuite {
+}
